60b33887195cf18da1fa64a2acebca3ea269386a
Krishnak96270 <krishnak96270@gmail.com>
2026-04-13 12:52:24 +0530

    Delete lib directory

--- lib/services/detection_service.dart
+++
@@ -1,9 +0,0 @@
-import 'package:sensors_plus/sensors_plus.dart';
-
-class DetectionService {
-  static void start(Function onDetect) {
-    accelerometerEvents.listen((e) {
-      if (e.z > 15) onDetect();
-    });
-  }
-}
